74c9f9b7febc41307be0b20015fe6f1bf0f1c63f
tuziel <tuziel@outlook.com>
2019-05-05 22:25:36 +0800

    update: 添加 loader 的 demo

--- src/c6-sprite/index.ts
+++ src/c6-sprite/index.ts
@@ -1,5 +1,6 @@
 import '../lib/polyfill/requestAnimationFrame';
-import Loop from '../lib/game/loop';
+// import Loop from '../lib/game/loop';
+import ImageLoader from '../lib/loader/imageLoader';
 
 window.addEventListener('load', (): void => {
   // 判断兼容性
@@ -9,57 +10,15 @@ window.addEventListener('load', (): void => {
   }
 
   // 获取canvas
-  const app = document.getElementById('app') as HTMLCanvasElement;
-  const context = app.getContext('2d')!;
-  const appWidth = app.width;
-  const appHeight = app.height;
+  // const app = document.getElementById('app') as HTMLCanvasElement;
+  // const context = app.getContext('2d')!;
+  // const appWidth = app.width;
+  // const appHeight = app.height;
 
-  /** 步长 */
-  const step = 10;
-  console.log(`游戏步长为: ${step}`);
+  // const gameloop = new Loop();
 
-  /** 游戏数据 */
-  let count = 0;
-
-  /**
-   * 绘制背景
-   */
-  function drawBackground(): void {
-    // 背景
-    context.fillStyle = '#ffffaa';
-    context.fillRect(0, 0, appWidth, appHeight);
-
-    // 边框
-    context.strokeStyle = '#000000';
-    context.strokeRect(5, 5, appWidth - 10, appHeight - 10);
-  }
-
-  /**
-   * 更新数字
-   */
-  function updateCount() {
-    count += step;
-  }
-
-  /**
-   * 渲染数字
-   *
-   * @param detla 当前时间领先游戏时间的长度
-   */
-  function drawaNum(detla: number) {
-    const text = (count + detla).toFixed(2);
-
-    context.font = '40px sans-serif';
-    context.fillStyle = '#000000';
-    context.fillText(text, 20, 100);
-  }
-
-  const gameloop = new Loop(step);
-  gameloop.update(() => {
-    updateCount();
-  });
-  gameloop.render((detla) => {
-    drawBackground();
-    drawaNum(detla);
-  });
+  new ImageLoader(require('./tanks_sheet.png'))
+    .use((image) => {
+      document.body.append(image);
+    });
 });
