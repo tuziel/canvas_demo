a2be5d34fe56f4396bc83eef55f7a49607394aa3
tuziel <tuziel@outlook.com>
2019-05-07 02:31:40 +0800

    update: 默认属性参考

--- src/lib/painter/canvas.ts
+++ src/lib/painter/canvas.ts
@@ -460,3 +460,27 @@ export default class C {
     return this;
   }
 }
+
+/**
+ * 默认属性参考
+ *
+ * fillStyle: "#000000"
+ * filter: "none"
+ * font: "10px sans-serif"
+ * globalAlpha: 1
+ * globalCompositeOperation: "source-over"
+ * imageSmoothingEnabled: true
+ * imageSmoothingQuality: "low"
+ * lineCap: "butt"
+ * lineDashOffset: 0
+ * lineJoin: "miter"
+ * lineWidth: 1
+ * miterLimit: 10
+ * shadowBlur: 0
+ * shadowColor: "rgba(0, 0, 0, 0)"
+ * shadowOffsetX: 0
+ * shadowOffsetY: 0
+ * strokeStyle: "#000000"
+ * textAlign: "start"
+ * textBaseline: "alphabetic"
+ */
