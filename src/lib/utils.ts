0b3b9b5a113b82e17cba400d50bcdf0131c6059f
tuziel <tuziel@outlook.com>
2019-05-10 17:45:23 +0800

    update: 加个js的版本

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,10 @@ export function test(fn: () => any, times: number = 1): number {
   const cost = performance.now() - s;
   return cost;
 }
+
+// function test(fn, times) {
+//   var s = performance.now();
+//   times = times >>> 0;
+//   while (times--) fn();
+//   return performance.now() - s;
+// }
