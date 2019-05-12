9d4eaa7324c238e8cf5a947c40a1ce59e2482122
tuziel <tuziel@outlook.com>
2019-05-13 00:31:31 +0800

    update: 添加运动物体和刚体物件

--- src/lib/objects/IObject2d.ts
+++ src/lib/objects/IObject2d.ts
@@ -1,4 +1,17 @@
+/** 2D 物件 */
 interface IObject2d {
   /** 渲染物件 */
-  render(context: CanvasRenderingContext2D): void;
+  render(context: CanvasRenderingContext2D, detlaTicks: number, tickTime: number): void;
+}
+
+/** 运动物件 */
+interface ISportObject2d extends IObject2d {
+  /** 更新物件 */
+  update(ticks: number, tickTime: number): void;
+}
+
+/** 刚体物件 */
+interface ICollideObject2d extends IObject2d {
+  /** 碰撞检测 */
+  test(target: IObject2d): boolean;
 }
